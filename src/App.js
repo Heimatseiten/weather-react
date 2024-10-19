8ad8c4d334d58b716604dc5145dc49176d5ab542
Tine Lüttgen <christine.luettgen@gmail.com>
2024-10-19 12:42:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {" "}
         <h1>Let's get OFF the ordinary !!</h1>
       </header>
     </div>
